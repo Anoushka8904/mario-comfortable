ca834a674651d4f97242d8669ef2301736ee2711
Anoushka8904 <Anoushka8904@dubaicollege.org>
2019-09-18 11:47:59 +0400

    abc2

--- marioComfortable.c
+++ marioComfortable.c
@@ -8,22 +8,22 @@ int main(void)
   {
       printf("Enter height of pyramid (integer from 0-23):");
       scanf("%d", &height);
-      fflush(stdin);
+      fflush(stdin); //prevents infinite loop when string is entered
   }
-  while (height < 0 || height > 23);
+  while (height < 0 || height > 23); //accepts int value from 0-23
 
   for (int i = 0; i < height; i++)
     {
-        for (int space = height - i; space > 1; space--)
+        for (int space = height - i; space > 1; space--) //loop prints spaces to align pyramids with right edge of window
         {
             printf(" ");
         }
-        for (int hash = 0; hash < i + 1; hash++)
+        for (int hash = 0; hash < i + 1; hash++) //loop prints hashes for 1st pyramid half
         {
             printf("#");
         }
-        printf("  ");
-	for (int hash = 0; hash < i + 1; hash++)
+        printf("  "); //double space between pyramid halves
+	for (int hash = 0; hash < i + 1; hash++) //loop prints hashes for 2nd pyramid half
         {
             printf("#");
         }
